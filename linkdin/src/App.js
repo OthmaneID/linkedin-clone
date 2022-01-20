bfe2f9988042c6c9c1e7d01ea0674b961204f3e5
OthmaneID <othmanid5@users.noreply.github.com>
2022-01-20 23:33:06 +0100

    leftSide Home designing commit

--- linkdin/src/App.js
+++ linkdin/src/App.js
@@ -2,15 +2,25 @@ import "./App.css";
 import Login from "./components/login";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-
-          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/" element={<Login />} />
+          
+          <Route
+            exact
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
